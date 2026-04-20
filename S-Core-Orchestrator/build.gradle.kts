3908c506da91f2fd0c3dbf4bb83ebd13d2c62f04
JJM <jjmyoucandoit@gmail.com>
2026-04-20 16:17:21 +0900

    mM

---
+++ S-Core-Orchestrator/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.5.13"
+    id("io.spring.dependency-management") version "1.1.4"
+
+}
+
+group = "org.dev.ssc"
+version = "0.01-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+    mavenCentral()
+}
+
+configurations {
+    all {
+        exclude(group="org.springframework.boot", module="spring-boot-starter-logging")
+    }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
+    implementation("com.lmax:disruptor:3.4.4")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+
+}
+
+
+
+
+tasks.test {
+    useJUnitPlatform()
+}
